d23c54902edf5e83ec9aff09e3e0b306298fe11c
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2024-09-16 16:43:36 +0530

    static root

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# This should already exist
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add or update STATIC_URL and STATIC_ROOT
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
